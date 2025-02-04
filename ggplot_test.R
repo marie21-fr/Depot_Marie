434ab245d1a8455b60cfee6c4f677867fd13e8d2
Marie Faure <fauremarie021@gmail.com>
2025-02-04 11:45:19 +0100

    premier test

---
+++ ggplot_test.R
@@ -0,0 +1,43 @@
+## ggplot2
+
+#install.packages("ggplot2")
+#install.packages("dplyr")
+
+library(ggplot2)
+library(dplyr)
+
+# decouverte des données
+data("iris")
+
+data("starwars")
+dim(starwars)
+head(starwars)
+names(starwars)
+
+
+# test
+ggplot(data=iris,
+       mapping = aes(x=Petal.Length, y=Petal.Width, colour = Species))+
+  geom_point()+
+  facet_grid(rows=vars(Species))
+
+# graphique
+ggplot(data=starwars,
+       mapping = aes(x=species, y=height, colour = gender))+
+  geom_boxplot()+
+  facet_grid(rows=vars(gender))
+
+
+ggplot(data=starwars,
+       mapping = aes(x=gender, y=height, colour = gender))+
+  geom_boxplot()+
+  geom_point()
+
+
+ggplot(data=starwars,
+       mapping = aes(x=eye_color, y=height, colour = gender))+
+  theme_dark()+
+  geom_boxplot()+
+  geom_point()+
+  labs(x = "couleur yeux", y = "taille",
+       title ="Starwaaaaaars")
